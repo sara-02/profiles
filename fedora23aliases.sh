0c5f28e427eeae78537576011312aaf0455236e3
Sarah <sarah@dhcp35-77.lab.eng.blr.redhat.com>
2016-06-09 11:10:37 +0530

    Fedora23

---
+++ fedora23aliases.sh
@@ -0,0 +1,6 @@
+alias subl="/home/sarah/sublime_text_3/sublime_text" #path for sublime text
+alias pycharm="/home/sarah/pycharm-community-5.0.4/bin/pycharm.sh" # path for pycharm
+alias jsonview="python -m json.tool" #pretty json viewer, command jsonview <filename>
+alias jsoncolor="pygmentize -l javascript" # colored pretty json, command jsonview | jsoncolor
+alias getthis="sudo dnf install" #shorcut for installing through rpm
+alias findhis="sudo dnf search" # shortcut to check through the rpm
