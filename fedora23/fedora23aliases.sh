4bc44cd434b9b39b614248b84511723d146680fe
sara-02 <sarahmasud02@gmail.com>
2016-06-22 21:45:15 +0530

    fedora alias updates

--- fedora23/fedora23aliases.sh
+++ fedora23/fedora23aliases.sh
@@ -1,6 +1,24 @@
-alias subl="/home/sarah/sublime_text_3/sublime_text" #path for sublime text
-alias pycharm="/home/sarah/pycharm-community-5.0.4/bin/pycharm.sh" # path for pycharm
+alias subl="/home/sarah/softwares/sublime_text_3/sublime_text" #path for sublime text
+alias pycharm="/home/sarah/softwares/pycharm-community-5.0.4/bin/pycharm.sh" # path for pycharm
 alias jsonview="python -m json.tool" #pretty json viewer, command jsonview <filename>
 alias jsoncolor="pygmentize -l javascript" # colored pretty json, command jsonview | jsoncolor
-alias getthis="sudo dnf install" #shorcut for installing through rpm
-alias findhis="sudo dnf search" # shortcut to check through the rpm
+alias getit="sudo dnf install" #shorcut for installing through rpm
+alias findit="sudo dnf search" # shortcut to check through the rpm
+alias hist="history"
+alias histgp='history | grep'
+alias ll="ls-alt"
+alias pingo='ping 8.8.8.8'
+
+##git aliases
+alias gs="git status"
+alias gA='git add'
+alias ga='git add -A'
+alias gc='git commit -m'
+alias gr='git remote -v'
+alias gpush='git push origin master'
+alias gpull='git pull origin master'
+alias gbd='git branch -b'
+alias gbD='git branch -D'
+alias gbush='git push origin :'
+alias grr='git remote remove '
+alias greset='git set-url origin'
