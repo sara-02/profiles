24622d849d7390212cb957655ea93ac08425922d
sara-02 <sarahmasud02@gmail.com>
2016-06-24 13:06:05 +0530

    Aliases updated

--- fedora23/fedora23aliases.sh
+++ fedora23/fedora23aliases.sh
@@ -8,17 +8,3 @@ alias hist="history"
 alias histgp='history | grep'
 alias ll="ls-alt"
 alias pingo='ping 8.8.8.8'
-
-##git aliases
-alias gs="git status"
-alias gA='git add'
-alias ga='git add -A'
-alias gc='git commit -m'
-alias gr='git remote -v'
-alias gpush='git push origin master'
-alias gpull='git pull origin master'
-alias gbd='git branch -b'
-alias gbD='git branch -D'
-alias gbush='git push origin :'
-alias grr='git remote remove '
-alias greset='git set-url origin'
