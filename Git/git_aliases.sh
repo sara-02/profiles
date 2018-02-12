7b9d958566f32337383cd8434d2a39cb1b0d1969
sara-02 <sarahmasud02@gamil.com>
2018-02-12 23:34:24 +0530

    Add new alias

--- Git/git_aliases.sh
+++ Git/git_aliases.sh
@@ -1,9 +1,12 @@
-##git aliases
+##Git aliases
+#!/bin/bash
+
 alias gs='git status'
-alias gp='git add -p'
+alias ga='git add -p'
 alias gc='git commit -m'
 alias gr='git remote -v'
 alias gd='git diff'
 alias gdc='git diff --cached'
 alias gst='git stash'
 alias gsp='git stash pop'
+alias gp='git push origin'
